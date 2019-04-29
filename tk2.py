a2209942fbc1c6986287370bdf0a6a53e0a2d333
akasoul <antonvoloshuk@gmail.com>
2019-04-29 22:33:36 +0300

    1212

--- tk2.py
+++ tk2.py
@@ -22,15 +22,17 @@ class app:
             a = np.genfromtxt(fpath)
         except:
             print("error")
-        print(a)
-        v = StringVar()
-        Label(self.lbl, textvariable=v).pack()
-
-        v.set(a)
+        else:
+            print(a)
+            self.lbl['text']=a
+            #v = StringVar()
+            #Label(self.lbl, textvariable=v).pack()
+            #v.set(a)
 
     def __init__(self):
         self.root = Tk()
         self.root.minsize(500,500)
+
         self.lbl=Label(self.root,  # родительское окно
                      text="Click me",  # надпись на кнопке
                      width=10, height=5,  # ширина и высота
@@ -40,11 +42,14 @@ class app:
                      width=10, height=5,  # ширина и высота
                      bg="white", fg="black")  # цвет фона и надписи
         self.dpath = Text(self.root,height=7,width=7,font='Arial 14',wrap=WORD)
+
         self.dpath.insert(1.0,'data.txt')
         self.btn.bind("<Button 1>",self.press_button)
+
         self.dpath.place(x=10, y=200)
         self.btn.place(x=10, y=5)
         self.lbl.place(x=100, y=200)
+
         self.root.mainloop()
 
 
