9aa489a2507304db49d7e4933d0caaa108bf6694
akasoul <32989026+akasoul@users.noreply.github.com>
2019-04-30 12:30:04 +0300

    123

--- tk2.py
+++ tk2.py
@@ -5,13 +5,36 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 import threading
-
+import os
+import time
 a = 2
 
 
 #a=2  # type: int
 
 class app:
+
+    def checkpath_thread(self):
+        print('thread start')
+        while(True):
+            if(self.new_path==True):
+                if(os.path.isfile(self.s_indatapath)):
+                    self.ed_indatapath['bg']= 'green'
+                else:
+                    self.ed_indatapath['bg'] = 'red'
+
+                if(os.path.isfile(self.s_outdatapath)):
+                    self.ed_outdatapath['bg']= 'green'
+                else:
+                    self.ed_outdatapath['bg'] = 'red'
+
+                if(os.path.isfile(self.s_inputpath)):
+                    self.ed_inputpath['bg']= 'green'
+                else:
+                    self.ed_inputpath['bg'] = 'red'
+
+                self.new_path=False
+
 
     def press_button(self,event):
         fpath=self.dpath.get(1.0,END)
@@ -29,28 +52,77 @@ class app:
             #Label(self.lbl, textvariable=v).pack()
             #v.set(a)
 
+
+    def on_changed(self,event):
+        fpath=self.ed_indatapath.get(1.0, END)
+        fpath=fpath.rstrip()
+        fpath=fpath.lstrip()
+        if(self.s_indatapath!=fpath):
+            self.s_indatapath = fpath
+            self.new_path=True
+
+        fpath=self.ed_outdatapath.get(1.0, END)
+        fpath=fpath.rstrip()
+        fpath=fpath.lstrip()
+        if(self.s_outdatapath!=fpath):
+            self.s_outdatapath = fpath
+            self.new_path=True
+
+        fpath=self.ed_inputpath.get(1.0, END)
+        fpath=fpath.rstrip()
+        fpath=fpath.lstrip()
+        if(self.s_inputpath!=fpath):
+            self.s_inputpath = fpath
+            self.new_path=True
+
+
     def __init__(self):
         self.root = Tk()
         self.root.minsize(500,500)
 
-        self.lbl=Label(self.root,  # родительское окно
-                     text="Click me",  # надпись на кнопке
-                     width=10, height=5,  # ширина и высота
-                     bg="white", fg="black")  # цвет фона и надписи
-        self.btn = Button(self.root,  # родительское окно
-                     text="Click me",  # надпись на кнопке
-                     width=10, height=5,  # ширина и высота
-                     bg="white", fg="black")  # цвет фона и надписи
-        self.dpath = Text(self.root,height=7,width=7,font='Arial 14',wrap=WORD)
+        self.s_indatapath=''
+        self.s_outdatapath=''
+        self.s_inputpath=''
 
-        self.dpath.insert(1.0,'data.txt')
-        self.btn.bind("<Button 1>",self.press_button)
+        self.new_path=True
 
-        self.dpath.place(x=10, y=200)
-        self.btn.place(x=10, y=5)
-        self.lbl.place(x=100, y=200)
+        self.lbl_indatapath=    Label(self.root,height=1,width=12,font='Arial 11',bg="white", fg="black",text='in_data fname :',anchor=W, justify=LEFT)
+        self.lbl_outdatapath=   Label(self.root,height=1,width=12,font='Arial 11',bg="white", fg="black",text='out_data fname:',anchor=W, justify=LEFT)
+        self.lbl_inputpath=     Label(self.root,height=1,width=12,font='Arial 11',bg="white", fg="black",text='input fname   :',anchor=W, justify=LEFT)
+        #self.btn = Button(self.root,  # родительское окно
+        #             text="Click me",  # надпись на кнопке
+        #             width=10, height=5,  # ширина и высота
+        #             bg="white", fg="black")  # цвет фона и надписи
+        self.ed_indatapath  = Text(self.root, height=1, width=15, font='Arial 11', wrap=WORD)
+        self.ed_outdatapath = Text(self.root, height=1, width=15, font='Arial 11', wrap=WORD)
+        self.ed_inputpath   = Text(self.root, height=1, width=15, font='Arial 11', wrap=WORD)
+
+        self.ed_indatapath.insert(1.0, 'in_data.txt')
+        self.ed_outdatapath.insert(1.0, 'out_data.txt')
+        self.ed_inputpath.insert(1.0, 'input.txt')
+
+        self.ed_indatapath.bind('<Key>', self.on_changed)
+        self.ed_outdatapath.bind('<Key>', self.on_changed)
+        self.ed_inputpath.bind('<Key>', self.on_changed)
+        self.ed_indatapath.bind('<Button 1>', self.on_changed)
+        self.ed_outdatapath.bind('<Button 1>', self.on_changed)
+        self.ed_inputpath.bind('<Button 1>', self.on_changed)
+
+        self.ed_indatapath.place(x=120, y=10)
+        self.ed_outdatapath.place(x=120, y=40)
+        self.ed_inputpath.place(x=120, y=70)
+
+        #self.btn.place(x=10, y=5)
+        self.lbl_indatapath.place(x=10, y=10)
+        self.lbl_outdatapath.place(x=10, y=40)
+        self.lbl_inputpath.place(x=10, y=70)
+
+        t = threading.Thread(target=self.checkpath_thread)
+        t.daemon = True
+        t.start()
 
         self.root.mainloop()
+
 
 
 z = app()
