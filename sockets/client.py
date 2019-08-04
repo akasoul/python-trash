a4652d20682ebb9cdc92bc982780dfb508c1d94b
antonvoloshuk <antonvoloshuk@gmail.com>
2019-08-04 23:04:14 +0300

    0206

--- sockets/client.py
+++ sockets/client.py
@@ -2,12 +2,13 @@ import socket
 
 
 addr='194.67.87.166'
+#addr='localhost'
 
 
 
 sock = socket.socket()
-sock.connect((addr, 9090))
-sock.send(b'a89skiiurjksa')
+sock.connect((addr, 9071))
+sock.send(b'qwerty ewqewq')
 
 data = sock.recv(1024)
 sock.close()
--- sockets/server.py
+++ sockets/server.py
@@ -1,23 +1,64 @@
 import socket
+import time
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-while True:
+class SockConnection:
 
+    def __init__(self,_port,_log_fname,_code):
+        self.is_opened=False
+        self.log_fname=_log_fname
+        self.code=_code
+        self.port=_port
+        try:
+            self.sock = socket.socket()
+        except:
+            pass
+        else:
+            self.is_opened=True
+        if(self.is_opened==True):
+            try:
+                self.sock.bind(('', self.port))
+            except:
+                pass
+            try:
+                self.sock.listen(1)
+            except:
+                pass
+            self.Loop()
 
-        conn, addr = sock.accept()
+    def Log(self,data):
+        file = open(self.log_fname, 'a+')
+        file.write(data)
+        file.close()
 
-        print('connected:', addr)
-
-        while True:
+    def AnswerThread(self,conn,addr):
+        try:
             data = conn.recv(1024)
-            if not data:
-                break
-            pwd=data[0:6].decode()
-            data=data[6:len(data)].decode()
-            if(pwd=='a89ski'):
-                data=data.encode()
-                conn.send(data.upper())
+        except:
+            pass
+        else:
+            pwd = data[0:6].decode()
+            data = data[6:len(data)].decode()
+            if (pwd == self.code):
+                self.Log(" " + data)
+                ans = str(addr) + str(data)
+                ans = ans.encode()
+                conn.send(ans.upper())
+                conn.close()
 
-        conn.close()
+    def Loop(self):
+        while True:
+            try:
+                conn,addr=self.sock.accept()
+            except:
+                pass
+            else:
+                self.Log("\n"+str(time.ctime())+" "+str(addr))
+                print('connected:', addr)
+                tt = threading.Thread(target=self.AnswerThread, args=(conn, addr))
+                tt.daemon = True
+                tt.start()
+
+            continue
+
+z=SockConnection(9071,"log.txt","qwerty")
