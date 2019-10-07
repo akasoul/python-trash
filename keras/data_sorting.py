b7ea38307fb0111b2e1c74997591b8b4a876d7ea
akasoul <32989026+akasoul@users.noreply.github.com>
2019-10-07 16:25:29 +0300

    123123

--- keras/data_sorting.py
+++ keras/data_sorting.py
@@ -23,23 +23,30 @@ filenames_out=np.array(
 for i in range(filenames_in.size):
     os.chdir(filenames_in[i])
     for file1 in os.listdir():
+
         if(os.path.isfile(file1)):
             shutil.copy(filenames_in[i]+file1,filenames_out[i]+file1)
         if(os.path.isdir(file1)):
+
             os.chdir(file1)
             file1+="/"
             for file2 in os.listdir():
+
                 if (os.path.isfile(file2)):
                     shutil.copy(filenames_in[i] +file1+ file2, filenames_out[i] +file1+ file2)
                 if (os.path.isdir(file2)):
+
                     os.chdir(file2)
                     file2+="/"
+
                     for file3 in os.listdir():
                         if (os.path.isfile(file3)):
                             shutil.copy(filenames_in[i] +file1+ file2+ file3, filenames_out[i] +file1+ file2+ file3)
                         if (os.path.isdir(file3)):
+
                             os.chdir(file3)
                             file3+="/"
+
                             for file4 in os.listdir():
                                 if (os.path.isfile(file4)):
                                     shutil.copy(filenames_in[i] +file1+ file2+file3+ file4, filenames_out[i] +file1+ file2+file3+ file4)
