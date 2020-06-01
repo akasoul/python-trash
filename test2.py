d729939fcb3bf82147ad7235e0806f146fef3560
akasoul <32989026+akasoul@users.noreply.github.com>
2020-06-01 09:09:35 +0300

    123123

--- test2.py
+++ test2.py
@@ -1,39 +1,6 @@
 import os
-import matplotlib.pyplot as plt
-import math
-import numpy as np
 
-
-a=tuple([1])
-b=(2,3,4)
-c=list(a)
-for i in b:
-    c.append(i)
-c=tuple(c)
-pass
-
-a1=[1,2,3]
-b1=[1]
-pass
-
-o=(1,)
-pass
-
-ds=tuple(1)
-pass
-#a = None
-#step = 2*3.14/100
-#for i in range(0,5000):
-#    value = 1000*(1+math.sin(0+step*i))
-#    if i == 0:
-#        a=np.array([value])
-#    else:
-#        a=np.append(a,value)
-#
-#fig = plt.figure(num='Plot', figsize=(16, 9), dpi=100)
-#plot = fig.add_subplot(111)
-#plot.plot(a, linewidth=0.5, color='b')
-#fig.show()
-#np.savetxt("D:/sinwave.txt" , a, delimiter=" ", fmt='%1.3f')
-#pass
-##testingfig.savefig(fname=self.job_dir + 'logs/test/' + ctime + '/all')
+a=os.listdir()
+for i in a:
+    if i.find(".png")<0:
+        os.rename(i,i+".png")
