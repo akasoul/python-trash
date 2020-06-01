0a48764ab60c29881d8bb6daefbaa12f4c84a3fa
antonvoloshuk <antonvoloshuk@gmail.com>
2020-06-01 06:53:14 +0300

    0206

--- keras/keras_gcloudai/trainer/modelMI.py
+++ keras/keras_gcloudai/trainer/modelMI.py
@@ -1874,3 +1874,19 @@ if __name__ == "__main__":
 
 
 
+
+
+#--job-dir=C:/Users/antonvoloshuk/AppData/Roaming/MetaQuotes/Terminal/287469DEA9630EA94D0715D755974F1B/tester/files/jobr/EURUSD/
+#--mode=2
+#--ctr=0
+#--data-size=2500
+#--eval-size=0.2
+#--batch-size=0.0256
+#--epochs=100000
+#--overfit-epochs=5000
+#--reduction-epochs=2000
+#--ls-reduction-koef=0.85
+#--ls=0.001
+#--l1=0.00
+#--l2=0.0001
+#--drop-rate=0.15
