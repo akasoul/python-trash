e540292ef3b0994f68b6894c1ffef6868cc0df49
akasoul <32989026+akasoul@users.noreply.github.com>
2020-06-09 15:27:11 +0300

    123123

--- keras/plotter.py
+++ keras/plotter.py
@@ -11,7 +11,7 @@ fig = plt.figure(num='fig', figsize=(16, 9), dpi=100)
 testingplot = fig.add_subplot(1,1,1)
 #trainingplot = fig.add_subplot(2,1,2)
 
-testingplot.plot(a[2798,0:99], linewidth=0.5, color='b')
-testingplot.plot(a[2798,100:200], linewidth=0.5, color='r')
+testingplot.plot(a[2799,0:99], linewidth=0.5, color='b')
+testingplot.plot(a[2799,100:200], linewidth=0.5, color='r')
 
 plt.show()
